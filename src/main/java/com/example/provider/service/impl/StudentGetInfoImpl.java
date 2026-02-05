7f91cbbf1ba610aa3809b6a362c7077c8589adff
leebean123 <lib@aixahc.com>
2026-02-05 13:06:43 +0800

    add service interface

--- src/main/java/com/example/provider/service/impl/StudentGetInfoImpl.java
+++ src/main/java/com/example/provider/service/impl/StudentGetInfoImpl.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Service;
 public class StudentGetInfoImpl implements StudentGetInfo {
     @Override
     public String getInfo(Long id) {
+
+        if (id == 1) {
+            return "是这条件分支测试";
+        }
         return "name:张三,age:20";
     }
 }
