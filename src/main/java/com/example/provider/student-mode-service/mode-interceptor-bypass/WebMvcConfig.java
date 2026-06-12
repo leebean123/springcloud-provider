224abbc8d9d80c4b54df1724e9144a451c18093a
leebean123 <lib@aixahc.com>
2026-06-12 17:58:21 +0800

    测试代码，删除注释

--- src/main/java/com/example/provider/student-mode-service/mode-interceptor-bypass/WebMvcConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.provider.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-
-}
